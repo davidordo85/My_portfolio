423e902dddddfbb0f76e312aba605da0449d6ec9
David Ordoñez <davidordonezescacena@hotmail.es>
2023-06-29 20:37:18 +0200

    Changes in navbar to improve visualization, styles.css removed and reset.css and navbar.css added

--- js/controller/NavbarController.js
+++ js/controller/NavbarController.js
@@ -3,8 +3,8 @@ import { navView } from '../views.js';
 
 export default class NavbarController extends BaseController {
   render() {
-    const nav = document.createElement('nav');
-    nav.innerHTML = navView();
-    this.element.appendChild(nav);
+    const navList = document.createElement('div');
+    navList.innerHTML = navView();
+    this.element.appendChild(navList);
   }
 }
