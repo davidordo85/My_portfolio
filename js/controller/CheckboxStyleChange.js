b135f1efc3c8a6f7104cbce7572be0137877a3f8
David Ordoñez <davidordonezescacena@hotmail.es>
2023-07-11 18:56:37 +0200

    changed contact section to footer

--- js/controller/CheckboxStyleChange.js
+++ js/controller/CheckboxStyleChange.js
@@ -11,14 +11,14 @@ export default class CheckboxStyleChange extends BaseController {
     this.element.addEventListener('change', event => {
       if (event.target.checked) {
         color = 'light';
-        this.styleNavbar(color);
+        this.styleNavbarAndFooter(color);
         this.textColor(color);
         this.bodyBackground(color);
         this.sectionBackground(color);
         this.projectBackground(color);
       } else {
         color = 'dark';
-        this.styleNavbar(color);
+        this.styleNavbarAndFooter(color);
         this.textColor(color);
         this.bodyBackground(color);
         this.sectionBackground(color);
@@ -27,12 +27,15 @@ export default class CheckboxStyleChange extends BaseController {
     });
   }
 
-  styleNavbar(color) {
+  styleNavbarAndFooter(color) {
     const navbar = document.querySelector('.navbar');
+    const footer = document.querySelector('.footer');
     if (color === 'light') {
       navbar.style.background = 'linear-gradient(#F8F9FA, #D3D3D3)';
+      footer.style.background = 'linear-gradient(#D3D3D3, #F8F9FA)';
     } else {
       navbar.style.removeProperty('background');
+      footer.style.removeProperty('background');
     }
   }
 
@@ -58,9 +61,8 @@ export default class CheckboxStyleChange extends BaseController {
 
   sectionBackground(color) {
     const description = document.querySelector('.description');
-    const contact = document.querySelector('.contact-container');
     const project = document.querySelector('.projects-container');
-    const sectionElements = [description, contact, project];
+    const sectionElements = [description, project];
     sectionElements.forEach(section => {
       if (color === 'light') {
         section.style.background = '#CCCCCC';
