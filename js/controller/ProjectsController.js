f9a461cdff5e0b8145c3507965e57f84bc4886ec
David Ordoñez <davidordonezescacena@hotmail.es>
2023-07-12 22:30:51 +0200

    JSON file loading from ProjectsController

--- js/controller/ProjectsController.js
+++ js/controller/ProjectsController.js
@@ -1,3 +1,24 @@
 import BaseController from './BaseController.js';
 
-export default class ProjectsController extends BaseController {}
+export default class ProjectsController extends BaseController {
+  renderProjects(projects) {
+    for (const project of projects) {
+      const article = document.createElement('article');
+      const projectHTML = projectsView(project);
+      article.innerHTML = projectHTML;
+    }
+  }
+
+  async readJSONFILE(file) {
+    try {
+      const response = await fetch(file);
+      if (!response.ok) {
+        throw new Error('Error en la carga del archivo JSON');
+      }
+      const jsonData = await response.json();
+      console.log(jsonData);
+    } catch (error) {
+      console.log('Error: ', error);
+    }
+  }
+}
--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 import CheckboxStyleChange from './controller/CheckboxStyleChange.js';
 import NavbarRenderController from './controller/NavbarController.js';
+import ProjectsController from './controller/ProjectsController.js';
 
 window.addEventListener('DOMContentLoaded', async event => {
   const nav = document.querySelector('.navbar-list');
@@ -8,4 +9,8 @@ window.addEventListener('DOMContentLoaded', async event => {
 
   const checkbox = document.querySelector('.checkbox');
   new CheckboxStyleChange(checkbox);
+
+  const projectsRender = document.querySelector('.projects-container');
+  const projects = new ProjectsController(projectsRender);
+  projects.readJSONFILE('../data/projects.json');
 });
